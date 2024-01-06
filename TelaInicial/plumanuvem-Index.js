4621a6f65eb7ac55f77d36e0c07471a8e1f3183b
rickshawwalis <rickshawwallis@gmail.com>
2024-01-06 02:09:20 -0300

    x

--- TelaInicial/plumanuvem-Index.js
+++ TelaInicial/plumanuvem-Index.js
@@ -1,140 +1,141 @@
 // PARTE PLUMA NUVEM
 //ABRE AS OPÇÕES DE CORES DE TAMANHOS
 const comprar = () => {
-  let adquirir = document.querySelector(".adquirir");
-  let mostrar = document.querySelector("#mostrarTudo");
-
-  if (adquirir) {
-    mostrar.style.display = 'block';
-  }
-  mostrar.scrollIntoView({ behavior: 'smooth' });
-
-}
-//LIMPA TUDO QUANDO CLICAR NO BOTÃO CANCELAR
-const limparSelecoes = () => {
-  const todosBotoesCores = document.querySelectorAll(".cores");
-  todosBotoesCores.forEach(botao => {
-    botao.style.backgroundColor = "";
-  });
-
-  const todosBotoesTamanhos = document.querySelectorAll(".tamanhos");
-  todosBotoesTamanhos.forEach(botao => {
-    botao.style.backgroundColor = "";
-  });
-
-  let imagemSapato = document.querySelector("#imagemSapato");
-  imagemSapato.innerHTML = "";
-
+    let adquirir = document.querySelector(".adquirir");
+    let mostrar = document.querySelector("#mostrarTudo");
   
-//limpar opção de quantidade escolhida
-  let select = document.getElementById("opcoes");
-  select.value = "";
-}
-
-//FECHAR
-const cancelar = () => {
-  let tituloA = document.querySelector(".tituloA")
-  let ocultar = document.querySelector(".cancelar");
-  let mostrar = document.querySelector("#mostrarTudo");
-  if (ocultar) {
-    mostrar.style.display = 'none';
+    if (adquirir) {
+      mostrar.style.display = 'block';
+    }
+    mostrar.scrollIntoView({ behavior: 'smooth' });
+  
   }
-  tituloA.scrollIntoView({ behavior: 'smooth' })
-  limparSelecoes()
-}
-
-
-//FAZ AS IMAGENS SURGIR
-document.addEventListener('click', (e) => {
-  const el = e.target;
-
-  //cria a imagem
-  let imagem = document.createElement('img');
-  imagem.setAttribute("id", "fotosImagem");
-  let imagemSapato = document.querySelector("#imagemSapato");
-
-  // Verifica se o elemento clicado é um botão de cor
-  if (el.classList.contains("cores")) {
-    const todosBotoes = document.querySelectorAll(".cores");
-    todosBotoes.forEach(botao => { //retira a cor quando clicado em outra
+  //LIMPA TUDO QUANDO CLICAR NO BOTÃO CANCELAR
+  const limparSelecoes = () => {
+    const todosBotoesCores = document.querySelectorAll(".cores");
+    todosBotoesCores.forEach(botao => {
       botao.style.backgroundColor = "";
     });
-
-    imagemSapato.innerHTML = "";
-
-    if (el.classList.contains("bege")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/lilas.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/lilas.png';
-      el.style.backgroundColor = "#A87AA9";
-    }
-    if (el.classList.contains("preto")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/preta.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/preta.png';
-      el.style.backgroundColor = "black";
-    }
-    if (el.classList.contains("amareloouro")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/amareloouro.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/amareloouro.png';
-      el.style.backgroundColor = "#E0BA12";
-    }
-    if (el.classList.contains("laranja")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/laranja.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/laranja.png';
-      el.style.backgroundColor = "#C84F22";
-    }
-    if (el.classList.contains("rosabebe")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/rosa.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/rosa.png';
-      el.style.backgroundColor = "#E29EAD";
-    }
-    if (el.classList.contains("branco")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/branca.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/branca.png';
-      el.style.backgroundColor = "white";
-    }
-    if (el.classList.contains("azulmarinho")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulmarinho.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulmarinho.png';
-      el.style.backgroundColor = "#666F8C";
-    }
-    if (el.classList.contains("azulclaro")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulbebe.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulbebe.png';
-      el.style.backgroundColor = "#61A2B0";
-    }
-    if (el.classList.contains("verdemilitar")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
-      el.style.backgroundColor = "#6F735C";
-    }
-
-    if (el.classList.contains("nude")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/nude.png';
-      el.style.backgroundColor = "#E3C9B9";
-    }
-
-    if (el.classList.contains("neon")) {
-      //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
-      imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/neon.png';
-      el.style.backgroundColor = "#BCE14F";
-    }
-    imagemSapato.appendChild(imagem);
-  }
-});
-
-//TAMANHOS
-document.addEventListener('click', (ev) => {
-  const et = ev.target;
-
-  if (et.classList.contains("tamanhos")) {
+  
     const todosBotoesTamanhos = document.querySelectorAll(".tamanhos");
-
-    todosBotoesTamanhos.forEach(botao => {// Retira a cor quando clicado em outra
+    todosBotoesTamanhos.forEach(botao => {
       botao.style.backgroundColor = "";
     });
-    // Define a cor apenas para o botão clicado
-    et.style.backgroundColor = "#61A2B0";
+  
+    let imagemSapato = document.querySelector("#imagemSapato");
+    imagemSapato.innerHTML = "";
+  
+    
+  //limpar opção de quantidade escolhida
+    let select = document.getElementById("opcoes");
+    select.value = "";
   }
-});
-
+  
+  //FECHAR
+  const cancelar = () => {
+    let tituloA = document.querySelector(".tituloA")
+    let ocultar = document.querySelector(".cancelar");
+    let mostrar = document.querySelector("#mostrarTudo");
+    if (ocultar) {
+      mostrar.style.display = 'none';
+    }
+    tituloA.scrollIntoView({ behavior: 'smooth' })
+    limparSelecoes()
+  }
+  
+  
+  //FAZ AS IMAGENS SURGIR
+  document.addEventListener('click', (e) => {
+    const el = e.target;
+  
+    //cria a imagem
+    let imagem = document.createElement('img');
+    imagem.setAttribute("id", "fotosImagem");
+    let imagemSapato = document.querySelector("#imagemSapato");
+  
+    // Verifica se o elemento clicado é um botão de cor
+    if (el.classList.contains("cores")) {
+      const todosBotoes = document.querySelectorAll(".cores");
+      todosBotoes.forEach(botao => { //retira a cor quando clicado em outra
+        botao.style.backgroundColor = "";
+      });
+  
+      imagemSapato.innerHTML = "";
+  
+      if (el.classList.contains("bege")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/lilas.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/lilas.png';
+        el.style.backgroundColor = "#A87AA9";
+      }
+      if (el.classList.contains("preto")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/preta.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/preta.png';
+        el.style.backgroundColor = "black";
+      }
+      if (el.classList.contains("amareloouro")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/amareloouro.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/amareloouro.png';
+        el.style.backgroundColor = "#E0BA12";
+      }
+      if (el.classList.contains("laranja")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/laranja.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/laranja.png';
+        el.style.backgroundColor = "#C84F22";
+      }
+      if (el.classList.contains("rosabebe")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/rosa.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/rosa.png';
+        el.style.backgroundColor = "#E29EAD";
+      }
+      if (el.classList.contains("branco")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/branca.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/branca.png';
+        el.style.backgroundColor = "white";
+      }
+      if (el.classList.contains("azulmarinho")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulmarinho.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulmarinho.png';
+        el.style.backgroundColor = "#666F8C";
+      }
+      if (el.classList.contains("azulclaro")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulbebe.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/azulbebe.png';
+        el.style.backgroundColor = "#61A2B0";
+      }
+      if (el.classList.contains("verdemilitar")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
+        el.style.backgroundColor = "#6F735C";
+      }
+  
+      if (el.classList.contains("nude")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/nude.png';
+        el.style.backgroundColor = "#E3C9B9";
+      }
+  
+      if (el.classList.contains("neon")) {
+        //imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/verdemilitar.png';
+        imagem.src = '/PedeAlgodao/img-Index/PLUMANUVEM/neon.png';
+        el.style.backgroundColor = "#BCE14F";
+      }
+      imagemSapato.appendChild(imagem);
+    }
+  });
+  
+  //TAMANHOS
+  document.addEventListener('click', (ev) => {
+    const et = ev.target;
+  
+    if (et.classList.contains("tamanhos")) {
+      const todosBotoesTamanhos = document.querySelectorAll(".tamanhos");
+  
+      todosBotoesTamanhos.forEach(botao => {// Retira a cor quando clicado em outra
+        botao.style.backgroundColor = "";
+      });
+      // Define a cor apenas para o botão clicado
+      et.style.backgroundColor = "#61A2B0";
+    }
+  });
+  
+  

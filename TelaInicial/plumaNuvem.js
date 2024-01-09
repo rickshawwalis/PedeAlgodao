6574d0404ca0c015b87bfd7a0a55b4722fcd5cf1
rickshawwalis <rickshawwallis@gmail.com>
2024-01-09 15:46:23 -0300

    cx

--- TelaInicial/plumaNuvem.js
+++ TelaInicial/plumaNuvem.js
@@ -184,15 +184,16 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
       //Ouvinte de evenxto para o botão "Concluir"
-      botaoEnviar.addEventListener("click", () => {
+     // botaoEnviar.addEventListener("click", () => {
         window.location.href = '/PedeAlgodao/CARRINHO/comprasFeitas.html';
-      });
- //   document.addEventListener('DOMContentLoaded', concluirCompra);
+     // });
+
   };
   document.querySelector('.concluir').addEventListener("click", concluirCompra)
+  
 })
 
-
+concluirCompra()
 
 
 

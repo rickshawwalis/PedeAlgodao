addd74c5129faf8fa7d056ad74e0d05a22ff68c7
rickshawwalis <rickshawwallis@gmail.com>
2024-01-11 19:35:53 -0300

    d

--- TelaInicial/plumaConfort-Index.js
+++ TelaInicial/plumaConfort-Index.js
@@ -164,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const concluirCompra = () => {
-    //estrutura para usar a condição de obrigar escolher cor, tamanho e opção
+    //estrutura para usar a coondição de obrigar escolher cor, tamanho e opção
     const coresSelecionadas = document.querySelector('.cores2:active');
     const tamanhoSelecionado = document.querySelector('.tamanhos2:active');
     const opcaoSelecionada = document.getElementById('opcoes2').value;
